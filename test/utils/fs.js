0db9e3ea178bf07a4f7bdb011c3e533c5338af13
Akshat <akshat.sh@gmail.com>
2018-07-10 23:16:35 -0700

    Task  tests (#5)
    * Added the first unit-test.
    
    * First test for utils/fs.
    
    * Replaced my own implementation of mkdir with a call to mkdir -p. Guess we wont be working on windows ¯\_(ツ)_/¯
    
    * Added some simple tests, and some pending tests.

---
+++ test/utils/fs.js
@@ -0,0 +1,52 @@
+const assert = require('assert');
+const mockery = require('mockery');
+const sinon = require('sinon');
+const nodeFS = require('fs');
+const glob = require('glob');
+
+describe('utils/fs', function() {
+  beforeEach(function () {
+    mockery.enable({
+      warnOnReplace: false,
+      warnOnUnregistered: false,
+      useCleanCache: true,
+    });
+  });
+  
+  afterEach(function() {
+    mockery.deregisterAll();
+    mockery.disable();
+  });
+
+  it('should promisify these functions', function() {
+    const expectedResolution = 'FOO';
+    const promisify = sinon.stub().returns(() => Promise.resolve(expectedResolution));
+    mockery.registerMock('./promisify', { promisify });
+    const {
+      expandSingleGlob,
+      readFile,
+      stat,
+      writeFile,
+    } = require('../../lib/utils/fs');
+
+    assert(typeof readFile === 'function', 'Expected a function exported as readFile');
+    assert.ok(promisify.calledWith(nodeFS.readFile));
+
+    assert(typeof stat === 'function', 'Expected a function exported as stat');
+    assert.ok(promisify.calledWith(nodeFS.stat));
+
+    assert(typeof writeFile === 'function', 'Expected a function exported as writeFile');
+    assert.ok(promisify.calledWith(nodeFS.writeFile));
+
+    assert(typeof expandSingleGlob === 'function', 'Expected a function exported as expandSingleGlob');
+    assert.ok(promisify.calledWith(glob));
+  });
+
+  describe('expandGlobs', function() {
+    it('should return a promise.');
+    it('should filter out undefined, and empty arrays.');
+    it('should return a flattened array.');
+  });
+
+  it('mkdir should call exec with mkdir -p');
+});
